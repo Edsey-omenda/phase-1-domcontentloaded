4372fe9cb4eee4ee73aeffe4e82ea5dced6db78b
Edsey omenda <omendajames0317@gmail.com>
2022-06-13 18:44:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("#text").innerHTML = "This is really cool!";
+})
+console.log(function(){})
+ 
+
+
+
+
+
+
+
+
+
+
+
